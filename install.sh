c94e696b07338c78025592846c51857e7655a7b8
ArcticTheRogue <arctictherogue@protonmail.com>
2019-12-11 21:00:34 -0500

    Added install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,34 @@
+#start
+echo Installing Arctic/dotfiles
+
+#bspwm
+cd ./bspwm
+mkdir ~/.config/bspwm
+cp -v ./bspwmrc ~/.config/bspwm
+cd ..
+
+#sxhkd
+cd ./sxhkd
+mkdir ~/.config/sxhkd
+cp -v ./sxhkdrc ~/.config/sxhkd
+cd ..
+
+#polybar
+cd ./polybar
+mkdir ~/.config/polybar
+cp -v ./config ~/.config/polybar
+cd ..
+
+#Xresources
+cd ./Xresources
+cp -v ./.Xresources ~
+cd ..
+
+#xinitrc
+cd ./xinitrc
+cp -v ./.xinitrc ~
+cd ..
+
+#ending
+echo Finished Installing
+exit
---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+#start
+echo Uninstalling Arctic/dotfiles
+
+#bspwm
+rm -rv ~/.config/bspwm
+
+#sxhkd
+rm -rv ~/.config/sxhkd
+
+#polybar
+rm -rv ~/.config/polybar
+
+#Xresources
+rm -rv ~/.Xresources
+
+#xinitrc
+rm -rv ./.xinitrc
+
+#ending
+echo Finished Uninstalling
+exit
